1b4a3e3b4c97c984451be73e6f3c332a6095c834
Snehal <snehalkc09@gmail.com>
2026-09-09 23:00:58 +0530

    Updated

---
+++ backend/students/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+from .models import Student
+
+# Create your views here.
+
+
+@csrf_exempt
+def register_student(request):
+
+    if request.method == "POST":
+
+        data = json.loads(request.body)
+
+        name = data.get("name")
+        email = data.get("email")
+        password = data.get("password")
+        address = data.get("address")
+
+        student = Student.objects.create(
+            name=name,
+            email=email,
+            password=password,
+            address=address
+        )
+
+        return JsonResponse({
+            "message": "Student registered successfully",
+            "id": student.id
+        })
+
+    return JsonResponse({
+        "message": "Only POST method is allowed"
+    })
